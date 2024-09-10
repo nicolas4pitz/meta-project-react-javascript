a8a544f1735a754f4620a51d28e5e253ab97a99f
nicolas4pitz <nicolas2004pitz@gmail.com>
2024-09-10 15:03:50 -0300

    finalizando primeira aula, resolvendo lógica do sql

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -2,12 +2,20 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider} from "fastify-type-provider-zod"; //importando o fastify-type-provider-zod que é um plugin para o fastify que vai fazer a validação dos dados e serialização
 import { createGoal } from "../functions/create-goal";
 import z from 'zod'
+import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
+import { createGoalCompletion } from "../functions/create-goal-completion";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.get('/pending-goals', async () => {
+    const { pendingGoals } = await getWeekPendingGoals()
+
+    return { pendingGoals }
+})
 
 app.post('/goals', {
     schema:{
@@ -22,6 +30,21 @@ app.post('/goals', {
     await createGoal({
         title,
         desiredWeeklyFrequency,
+    }) 
+})
+
+
+app.post('/completions', {
+    schema:{
+        body: z.object({  
+            goalId: z.string(),
+        })
+    }
+},async request => {
+    const { goalId } = request.body
+
+    await createGoalCompletion({
+        goalId,
     }) 
 })
 
