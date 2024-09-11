987551ed42304deb94f5d4e67d29a8ea79fa2645
nicolas4pitz <nicolas2004pitz@gmail.com>
2024-09-11 17:36:39 -0300

    Essa é basicamente a forma para ligar o back e front com tanstack

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,41 +1,26 @@
 // JSX JavaScript XML => HTML dentro do JavaScript
 
-import { useEffect, useState } from 'react'
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
 
 import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 //import { EmptyGoals } from './components/empty-goals'
 
-type SummaryResponse = {
-  completed: number;
-  total: number;
-  goalsPerDay: Record<string, {
-      id: string;
-      title: string;
-      completedAt: string;
-  }[]>;
-}
 
 export function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null>(null)
 
-  useEffect(() => {
-    fetch('http://localhost:3000/summary').then((response) => {
-      return response.json()
-    }).then((data) => {
-      setSummary(data.summary)
-    }) //essa linha
-  }, []) //toda vez que o que colocarmos dentro desse [] mudar, ele vai executar o useEffect
-  //Forma de Buscar dados do frontEnd para o BackEnd
-
-  console.log(summary)
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary
+    })
 
   return (
     <Dialog>
 
-      {summary?.total && summary.total > 0 ? <Summary /> : <EmptyGoals />}
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
       <CreateGoal />
     </Dialog>
